1b86114793f9cb8b02096372ede17caf80ad45b2
danielcampos <ddregalo@gmail.com>
2018-02-14 20:31:14 +0000

    New test alternative method adding to Class Integer

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,15 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "#{number} --> fizzbuzz"
-  elsif number % 3 == 0
-    "#{number} --> fizz"
-  elsif number % 5 == 0
-    "#{number} --> buzz"
-  else
-    "#{number} --> #{number}"
-  end
+
+module ExtraMethods
+def fizzbuzz
+    if self % 3 == 0 && self % 5 == 0
+      "fizzbuzz"
+    elsif self % 3 == 0
+      "fizz"
+    elsif self % 5 == 0
+      "buzz"
+    else
+      "#{self}"
+    end
 end
+end
+Integer.class_eval { include ExtraMethods }
